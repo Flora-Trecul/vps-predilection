571ebd0f9a5cb1bd14c401755a9b8038b0723e29
Flora T <flora.trecul@gmail.com>
2026-06-04 16:39:05 +0200

    fix deploy

--- django_political_app/django_political_app/settings.py
+++ django_political_app/django_political_app/settings.py
@@ -29,9 +29,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'app.docker.localhost', 'predilection.duckdns.org']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'app.docker.localhost', 'predi_lection-app.duckdns.org']
 
-CSRF_TRUSTED_ORIGINS = ['https://app.docker.localhost', 'https://predilection.duckdns.org']
+CSRF_TRUSTED_ORIGINS = ['https://app.docker.localhost', 'https://predi_lection-app.duckdns.org']
 
 # Application definition
 
